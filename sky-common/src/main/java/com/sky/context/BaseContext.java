aab351bf7a67ae5ca24d3d8a9a5ed3018805f3d0
32499 <3249921164@qq.com>
2025-05-07 15:36:04 +0800

    新增员工代码

--- sky-common/src/main/java/com/sky/context/BaseContext.java
+++ sky-common/src/main/java/com/sky/context/BaseContext.java
@@ -5,10 +5,12 @@ public class BaseContext {
     public static ThreadLocal<Long> threadLocal = new ThreadLocal<>();
 
     public static void setCurrentId(Long id) {
+
         threadLocal.set(id);
     }
 
     public static Long getCurrentId() {
+
         return threadLocal.get();
     }
 
